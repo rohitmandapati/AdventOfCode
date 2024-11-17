96910d4a1ea0fd69ee844bc0f37c8083122976b1
humano0o <rohitmandapati06@gmail.com>
2024-11-17 17:03:41 -0500

    2023, Day 4, Part 2

--- src/year23/day4/ScratchCard.java
+++ src/year23/day4/ScratchCard.java
@@ -6,18 +6,30 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ScratchCard {
     private int[] winningNumbers;
     private int[] numbers;
     private int score;
+    private int id;
+    private int matches;
 
-    public ScratchCard(int[] winningNumbers, int[] numbers) {
+    public ScratchCard(int[] winningNumbers, int[] numbers, int id) {
         this.winningNumbers = winningNumbers;
         this.numbers = numbers;
-        if (this.numWinners() == 0) this.score = 0;
+        this.matches = this.numWinners();
+        if (this.matches == 0) this.score = 0;
         else {
-            this.score = (int) Math.pow(2,this.numWinners() - 1);
+            this.score = (int) Math.pow(2,this.matches - 1);
         }
+        this.id = id;
+    }
+
+    // for Part 2
+    public ScratchCard(ScratchCard s) {
+        this(s.getWinners(), s.getNums(), s.getId());
     }
 
     private int numWinners() {
@@ -52,21 +64,51 @@ public class ScratchCard {
 
     private int getScore() { return this.score; }
 
+    //Part 2
+    private int getId() { return this.id; }
+    private int getMatches() { return this.matches; }
+    private int[] getNums() { return this.numbers; }
+    private int[] getWinners() { return this.winningNumbers; }
+
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src/year23/day4/input.txt");
         Scanner scan = new Scanner(file);
 
-        List cards = new ArrayList<ScratchCard>(218);
+        // for Part 2
+        List cardsList = new ArrayList<ScratchCard>();
+        Queue cardsQueue = new LinkedList<ScratchCard>();
+        
+        
         int totalScore = 0;
 
+        int id = 0;
         while(scan.hasNextLine()) {
             int[][] ret = parse(scan.nextLine());
-            ScratchCard scratch = new ScratchCard(ret[0], ret[1]);
+            ScratchCard scratch = new ScratchCard(ret[0], ret[1], id++);
             totalScore += scratch.getScore();
+
+            //for Part 2
+            cardsList.add(scratch);
+            cardsQueue.add(scratch);
         }
 
         System.out.println(totalScore);
 
+
+        // ------------ Part 2 ------------
+
+        int totalCards = 0;
+
+        while (!cardsQueue.isEmpty()) {
+            ScratchCard s = (ScratchCard) cardsQueue.remove();
+            totalCards++;
+
+            for (int i = 1; i <= s.getMatches(); i++) {
+                cardsQueue.add(cardsList.get(s.getId() + i));
+            }
+        }
+
+        System.out.println(totalCards);
 
     }
 }
