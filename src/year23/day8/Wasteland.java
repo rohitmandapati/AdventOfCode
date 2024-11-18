e63d7351499f7b2246405af0622eb88fa61cf276
humano0o <rohitmandapati06@gmail.com>
2024-11-17 22:43:22 -0500

    2023, Day 8, Part 2

--- src/year23/day8/Wasteland.java
+++ src/year23/day8/Wasteland.java
@@ -5,11 +5,17 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.HashMap;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Wasteland {
     
-    private static HashMap<String, String[]> map = new HashMap<String, String[]>();
+    public static HashMap<String, String[]> map = new HashMap<String, String[]>();
+    public static final char[] INSTRUCTIONS = readLine("src/year23/day8/instructions.txt").toCharArray();
 
-    private static final char[] INSTRUCTIONS = readLine("src/year23/day8/instructions.txt").toCharArray();
+    //for Part 2
+    public static List<String> entries = new ArrayList<String>();
 
     private static String readLine(String location) {
         File file = new File(location);
@@ -43,16 +49,110 @@ public class Wasteland {
         while (scan.hasNextLine()) {
             String[][] input = parse(scan.nextLine());
             map.put(input[0][0], input[1]);
+
+            // for Part 2
+            if(input[0][0].lastIndexOf('A') == 2) {
+                entries.add(input[0][0]);
+            }
+
         }
         scan.close();
     }
 
+    // for Part 2
+    public static boolean checkEnd(Object[] l, char ch) {
+        for (int i = 0; i < l.length; i++) {
+            if (!(l[i].toString().charAt(2) == ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private static boolean checkLast(String s, char ch) {
+        if (s.charAt(s.length() - 1) == ch) { return true; }
+        else { return false; }
+    }
+
+    public static long findGCD(long a, long b) {
+        if (b == 0)
+            return a;
+        return findGCD(b, a % b);
+    }
+
+    public static long findLCM(long a, long b) {
+        return a * b / findGCD(a, b);
+    }
+
+    public static long findLCMArray(int[] arr) {
+        long lcm = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            lcm = findLCM(lcm, arr[i]);
+        }
+        return lcm;
+    }
+
+    public static long lcm_of_array_elements(int[] element_array)
+    {
+        long lcm_of_array_elements = 1;
+        int divisor = 2;
+        
+        while (true) {
+            int counter = 0;
+            boolean divisible = false;
+            
+            for (int i = 0; i < element_array.length; i++) {
+
+                // lcm_of_array_elements (n1, n2, ... 0) = 0.
+                // For negative number we convert into
+                // positive and calculate lcm_of_array_elements.
+
+                if (element_array[i] == 0) {
+                    return 0;
+                }
+                else if (element_array[i] < 0) {
+                    element_array[i] = element_array[i] * (-1);
+                }
+                if (element_array[i] == 1) {
+                    counter++;
+                }
+
+                // Divide element_array by devisor if complete
+                // division i.e. without remainder then replace
+                // number with quotient; used for find next factor
+                if (element_array[i] % divisor == 0) {
+                    divisible = true;
+                    element_array[i] = element_array[i] / divisor;
+                }
+            }
+
+            // If divisor able to completely divide any number
+            // from array multiply with lcm_of_array_elements
+            // and store into lcm_of_array_elements and continue
+            // to same divisor for next factor finding.
+            // else increment divisor
+            if (divisible) {
+                lcm_of_array_elements = lcm_of_array_elements * divisor;
+            }
+            else {
+                divisor++;
+            }
+
+            // Check if all element_array is 1 indicate 
+            // we found all factors and terminate while loop.
+            if (counter == element_array.length) {
+                return lcm_of_array_elements;
+            }
+        }
+    }
+    
+
     public static void main(String[] args) throws FileNotFoundException {
         String inputLocation = "src/year23/day8/input.txt";
-        File input = new File("src/year23/day8/input.txt");
+        File input = new File(inputLocation);
         createHash(input);
 
-        // String entry = readLine(inputLocation).substring(0,3);
         String entry = "AAA";
 
         int steps = 0;
@@ -66,15 +166,41 @@ public class Wasteland {
             } else {
                 entry = map.get(entry)[1];
             }
-
-            // if (entry.equals("LHV")) break;
             steps++;
             index++;
-            System.out.println(entry);
         }
 
         System.out.println(steps);
 
+        // ------------ Part 2 ------------
+
+        steps = 0;
+        index = 0;
+
+        Object[] nodes__ = entries.toArray();
+        String[] nodes = new String[nodes__.length];
+        for (int i = 0; i < nodes__.length; i++) {
+            nodes[i] = (String) nodes__[i];
+        }
+
+        int[] result = new int[nodes.length];
+        for (int i = 0; i < nodes.length; i++) {
+            index = 0;
+            steps = 0;
+            while(!(checkLast(nodes[i], 'Z')) )  {
+                if (index == INSTRUCTIONS.length) {
+                    index = 0;
+                }
+                if (INSTRUCTIONS[index] == 'L') {
+                    nodes[i] = map.get(nodes[i])[0];
+                } else {
+                    nodes[i] = map.get(nodes[i])[1];
+                }
+                steps++;
+                index++;
+            }
+            result[i] = steps;
+        }
+        System.out.println(findLCMArray(result));
     }
-    
 }
